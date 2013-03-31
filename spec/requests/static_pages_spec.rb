4ea04717996ce44bd8f8a7216d02e052f01c56db
Justin McAdams <mcadamsrails@gmail.com>
2013-03-31 02:13:22 -0500

    finish chap 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -56,7 +56,7 @@ describe "Static pages" do
     page.should have_selector 'title', text: full_title('Contact')
     click_link "Home"
     click_link "Sign up now!"
-    page.should have_selector 'title', text: full_title('Sign Up')
+    page.should have_selector 'title', text: full_title('Sign up')
     click_link "sample app"
     page.should_not have_selector 'title', text: full_title(' | Home')
   end
