45d0e55a8e25de780f9907de026ace71ca87e418
Justin McAdams <mcadamsrails@gmail.com>
2013-04-07 00:39:18 -0500

    hopefully finish chap 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
   def show
   	@user = User.find(params[:id])
   end
 
   def new
   	@user = User.new
+  end
+
+  def index
+    @users = User.all
   end
 
   def create
@@ -17,5 +22,18 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 
 end
